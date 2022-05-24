6b42d46957b7cfa0548aca097226bf20dac33a77
aadiosap <aadiosap@unal.edu.co>
2022-05-24 05:35:50 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,11 +5,9 @@ Calificación del laboratorio
 
 import sys
 
-import soluciones
-
 # import preguntas
 preguntas = soluciones
-
+import preguntas
 
 def test_01():
     # ---< Input/Output test case >----------------------------------------------------
